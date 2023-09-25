5d5db330379464bdb99808e4649e9730d08a67d6
Kent Jones JR <jonesk6843@student.faytechcc.edu>
2023-09-25 11:45:40 -0400

    Layout of H3HW1(Silver)
    Not Complete.

---
+++ Jonesk_M3HW1(Q4)/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+/*
+CSC 134
+M3HW1 - Silver
+Kent Jones
+9/25/23
+*/
+using namespace std;
+
+int main()
+{
+    // Declare Variables
+
+    // Display Problem
+
+    // Determine if user input is correct
+    cout << "Hello world!" << endl;
+    return 0;
+}
