ae6c21cc0f5c26ed79a2585a1b278b61854b91b9
Kent Jones JR <jonesk6843@student.faytechcc.edu>
2023-09-27 10:27:21 -0400

    M3HW1(Q4) completed

--- Jonesk_M3HW1(Q4)/main.cpp
+++ Jonesk_M3HW1(Q4)/main.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
+#include <stdlib.h> /* srand, rand */
+#include <time.h> // for time()
 /*
 CSC 134
-M3HW1 - Silver
+M3HW1 - Gold
 Kent Jones
-9/25/23
+9/27/23
 */
 using namespace std;
 
 int main()
 {
     // Declare Variables
+    int seed = time(0);
+    srand(seed); // Passes to Generator
+    int randNum1 = rand()% 20 + 1;
+    int randNum2 = rand()% 20 + 1;
+    int sum = randNum1 + randNum2;
+    int userAnswer;
+    bool repeat = false;
 
     // Display Problem
+    while (repeat == false)
+    {
+        cout << randNum1 << " + " << randNum2 << " = " << " ? " << endl;
+        cout << "What is the Answer?" << endl;
+        cout << "> "; cin >> userAnswer;
 
-    // Determine if user input is correct
-    cout << "Hello world!" << endl;
+        // Determine if user input is correct
+        if (userAnswer == sum)
+        {
+            cout << "C O R R E C T !" << endl;
+            repeat = true;
+        }
+        else
+        {
+            cout << "I N C O R R E C T !" << endl;
+        }
+    }
     return 0;
 }
